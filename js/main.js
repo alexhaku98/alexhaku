626427110f5af69f4c7c66a5a94d5c2d71df59dd
Alex <trifu.alex@yahoo.com>
2026-01-07 18:01:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var w = screen.width;
+if (w > 768) {
+    function myFunction() {
+        var copyText = "+40735956217";
+        navigator.clipboard.writeText(copyText);
+        alert("Phone Number copied to clipboard");
+    }
+}
